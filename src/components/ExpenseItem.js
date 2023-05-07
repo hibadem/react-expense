69d6b588f7706305bcf91c281167b30655fc6210
halil <halilbadem47@gmail.com>
2023-05-07 08:55:55 +0300

    Composition, children props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,17 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
-function ExpenseItem() {
-    const expenseDate = new Date(2023, 1, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.98;
-
-    return (
-        <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
-            <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
-            </div>
-        </div>
-    );
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
